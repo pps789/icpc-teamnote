aab07647d1f89c5e7fcf13342e663a0ef5572b2a
Eu Ka On <wlxyzlw@bacchus.snucse.org>
2017-10-26 14:54:05 +0900

    jfio with byte

---
+++ src/miscellaneous/fio.java
@@ -0,0 +1,35 @@
+package fio;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class fio
+{
+	final private int BSIZE=524288;
+    private byte[] buffer = new byte[BSIZE];
+    private int p = BSIZE;
+    private InputStream in;
+    public byte readByte() throws IOException
+    {
+        if(p==BSIZE)
+        {
+            in.read(buffer);
+            p=0;
+        }
+        return buffer[p++];
+    }
+    public int readInt() throws IOException
+    {
+        byte b = readByte();
+        while (( b < '0' || b > '9') && b != '-') {
+          b = readByte();
+        }
+        int ret=0; boolean neg = b == '-';
+        if (neg) b = readByte();
+        while (b >= '0' && b <= '9') {
+          ret = ret * 10 + b - '0';
+          b = readByte();
+        }
+        return neg?-ret:ret;
+    }
+}
