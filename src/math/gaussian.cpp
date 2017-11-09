3d24ac57ef30fdb170443c028249b28664ef388a
iskim <iskim517@gmail.com>
2017-11-09 21:46:03 +0900

    Remove matrix operations

--- src/math/gaussian.cpp
+++ src/math/gaussian.cpp
@@ -8,17 +8,19 @@ typedef vector<vector<double>> VVD;
 // OUTPUT:   X      = an n*m matrix (stored in b[][])
 //           A^{-1} = an n*n matrix (stored in a[][])
 // O(n^3)
-bool gauss_jordan(VVD& a, VVD& b) {
+double gauss_jordan(VVD& a, VVD& b) {
     const int n = a.size();
     const int m = b[0].size();
     vector<int> irow(n), icol(n), ipiv(n);
+
+    double det = 1;
 
     for (int i = 0; i < n; i++) {
         int pj = -1, pk = -1;
         for (int j = 0; j < n; j++) if (!ipiv[j])
             for (int k = 0; k < n; k++) if (!ipiv[k])
                 if (pj == -1 || fabs(a[j][k]) > fabs(a[pj][pk])) { pj = j; pk = k; }
-        if (fabs(a[pj][pk]) < EPS) return false; // matrix is singular
+        if (fabs(a[pj][pk]) < EPS) return 0; // matrix is singular
         ipiv[pk]++;
         swap(a[pj], a[pk]);
         swap(b[pj], b[pk]);
@@ -26,6 +28,7 @@ bool gauss_jordan(VVD& a, VVD& b) {
         icol[i] = pk;
 
         double c = 1.0 / a[pk][pk];
+        det *= a[pk][pk];
         a[pk][pk] = 1.0;
         for (int p = 0; p < n; p++) a[pk][p] *= c;
         for (int p = 0; p < m; p++) b[pk][p] *= c;
@@ -39,5 +42,5 @@ bool gauss_jordan(VVD& a, VVD& b) {
     for (int p = n - 1; p >= 0; p--) if (irow[p] != icol[p]) {
         for (int k = 0; k < n; k++) swap(a[k][irow[p]], a[k][icol[p]]);
     }
-    return true;
+    return det;
 }
